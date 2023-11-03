403959fb23b10ae4e1559c5c08717da8be85613e
TrongHieu0405 <hieunguyentrong045@gmail.com>
2023-11-03 09:34:06 +0700

    update admin dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,73 @@
-import React, { Component } from 'react';
-import Nav from '../src/components/Nav';
-import Footer from '../src/components/Footer';
-import './style/style.scss'
-import { Outlet } from 'react-router-dom';
-class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <div className="App-header">
-                    <Nav />
-                </div>
-                <div className="App-body">
-                    <Outlet></Outlet>
-                </div >
-                <div className="App-footer">
-                    <Footer />
-                </div>
-            </div >
-        );
-    }
+import React, { Component } from "react";
+import Nav from "../src/components/Nav";
+import Footer from "../src/components/Footer";
+import "./style/style.scss";
+import { Outlet } from "react-router-dom";
+
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+import Team from "./scenes/team";
+import Invoices from "./scenes/invoices";
+import Contacts from "./scenes/contacts";
+import Bar from "./scenes/bar";
+import Form from "./scenes/form";
+import Line from "./scenes/line";
+import Pie from "./scenes/pie";
+import FAQ from "./scenes/faq";
+import Geography from "./scenes/geography";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+import Calendar from "./scenes/calendar/calendar";
+// class App extends Component {
+//     render() {
+//         return (
+//             <div className="App">
+//                 <div className="App-header">
+//                     <Nav />
+//                 </div>
+//                 <div className="App-body">
+//                     <Outlet></Outlet>
+//                 </div >
+//                 <div className="App-footer">
+//                     <Footer />
+//                 </div>
+//             </div >
+//         );
+//     }
+// }
+
+function App() {
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar isSidebar={isSidebar} />
+          <main className="content">
+            <Topbar setIsSidebar={setIsSidebar} />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/faq" element={<FAQ />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/geography" element={<Geography />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
-
 export default App;
-
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,35 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/bootstrap/dist/js/bootstrap';
-import App from './App';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import reportWebVitals from './reportWebVitals';
-import Home from './components/Home';
-import ContactUs from './components/ContactUs';
-import MyAccount from './components/MyAccount';
-import Login from './components/Login';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap";
+import App from "./App";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
+import Home from "./components/Home";
+import ContactUs from "./components/ContactUs";
+import MyAccount from "./components/MyAccount";
+import Login from "./components/Login";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<App />}>
-          <Route path='/' element={<Home />}></Route>
-          <Route path='contact' element={<ContactUs />}></Route>
-          <Route path='login' element={<Login />}></Route>
-          <Route path='myaccount' element={<MyAccount />}>
-            <Route path='account-general' element={<account-general />}></Route>
-            <Route path='account-change-password' element={<account-change-password />}>
-            </Route>
-
+      {/* <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="contact" element={<ContactUs />}></Route>
+          <Route path="login" element={<Login />}></Route>
+          <Route path="myaccount" element={<MyAccount />}>
+            <Route path="account-general" element={<account-general />}></Route>
+            <Route
+              path="account-change-password"
+              element={<account-change-password />}
+            ></Route>
           </Route>
         </Route>
-
-      </Routes>
+      </Routes> */}
+      {/* Admin */}
+      <App />
+      {/* Admin */}
     </BrowserRouter>
-  </React.StrictMode >
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
