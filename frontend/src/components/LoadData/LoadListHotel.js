19cd5eb4bd07878da3f83a19ec23c9ebc26c736b
Huynh Nhat Cuong <cuonghuynhnhat2025@gmail.com>
2023-11-26 16:54:59 +0700

    Creat folder LoadData and func LoadListHotel, LoadListProvince, Complete func Search

---
+++ frontend/src/components/LoadData/LoadListHotel.js
@@ -0,0 +1,39 @@
+import React, { createContext, useState, useEffect } from 'react';
+import axios from 'axios';
+
+// export const HotelContext = createContext();
+
+// const LoadListHotel = ({ children }) => {
+
+//     const searchById = async (id) => {
+//         try {
+//             const response = await axios.get(`http://localhost:8080/api/v1/hotels/provinces/${id}`);
+//             console.log("thành công :"+ response.data);
+//             return response.data.data;
+//         } catch (error) {
+//             console.error('Error fetching hotel by province id:', error);
+//             return [];
+//         }
+//     };
+
+//     return (
+//         <HotelContext.Provider value={{ searchById }}>
+//             {children}
+//         </HotelContext.Provider>
+//     );
+// };
+
+// export default LoadListHotel;
+
+
+const LoadListHotel = async (id) => {
+    try {
+        console.log(id);
+        const response = await axios.get(`http://localhost:8080/api/v1/hotels/provinces/${id}`);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching hotel list:', error);
+        return [];
+    }
+};
+export { LoadListHotel };
---
+++ frontend/src/components/LoadData/LoadListProvince.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+
+
+const LoadListProvince = async () => {
+    try {
+        const response = await axios.get('http://localhost:8080/api/v1/provinces');
+        return response.data.data;
+    } catch (error) {
+        console.error('Error fetching hotel list:', error);
+        return [];
+    }
+};
+export { LoadListProvince };
